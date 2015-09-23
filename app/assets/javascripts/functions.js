0314c9c1cdceef98fadb9a3849da83bb245d2d8f
HectorMg <hectormgwake@gmail.com>
2015-09-23 12:53:26 -0500

    Fix navbar animations on scroll, add About Me div

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -2,11 +2,14 @@
 $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
+  var links = document.getElementById('blackLinks');
 
-  if(wScroll > $('#greet').offset().top-80){
+  if(wScroll > $('#greet').offset().top-100){
     $('nav').addClass('opaqueNav');
+    $('#logo').addClass('nav-logo-black');
   }
-  if(wScroll < $('#greet').offset().top-80){
+  if(wScroll < $('#greet').offset().top-100){
     $('nav').removeClass('opaqueNav');
+    $('#logo').removeClass('nav-logo-black');
   }
 });
