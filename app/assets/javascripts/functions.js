be9ec8ae8450f2b304c3536a9118f1d76a39844f
HectorMg <hectormgwake@gmail.com>
2015-09-24 01:08:32 -0500

    Finish greeting section, restyle navbar, add About Me section and add Scrollit.js for scroll nav

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -4,12 +4,15 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
   var links = document.getElementById('blackLinks');
 
-  if(wScroll > $('#greet').offset().top-100){
+  if(wScroll > $('#greet').offset().top-200){
     $('nav').addClass('opaqueNav');
     $('#logo').addClass('nav-logo-black');
   }
-  if(wScroll < $('#greet').offset().top-100){
+  if(wScroll < $('#greet').offset().top-200){
     $('nav').removeClass('opaqueNav');
     $('#logo').removeClass('nav-logo-black');
   }
+});
+$(function(){
+  $.scrollIt();
 });
